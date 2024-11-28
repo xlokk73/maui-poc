3c484a26b57ef817d4588f68135743b89ee0bd6f
Manwel Bugeja <manwelbugeja23@icloud.com>
2024-11-28 15:34:09 +0100

    Added basic android library and default MAUI app

---
+++ HelloAndroidLibrary/app/src/main/java/com/example/helloandroidlibrary/HelloAndroid.kt
@@ -0,0 +1,7 @@
+package com.example.helloandroidlibrary
+
+class HelloAndroid {
+    fun sayHello(): String {
+        return "Hello, Android!"
+    }
+}
---
+++ MauiPoc/MauiPoc/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,8 @@
+﻿using Foundation;
+
+namespace MauiPoc;
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
